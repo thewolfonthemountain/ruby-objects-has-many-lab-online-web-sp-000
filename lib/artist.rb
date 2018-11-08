26b85b29a8091638283bca67d2fedb08b4b54410
Sean Langton <slangton1@gmail.com>
2018-11-08 03:59:35 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,7 +20,7 @@ attr_accessor :name
   end
 
   def add_song_by_name(song_name)
-    song_name = Song.new
+    song_name = Song.new(song_name)
     song_name.artist = self
     @songs << song_name
     @@song_count += 1
